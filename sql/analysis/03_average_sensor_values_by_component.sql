67361ff0f2608db721a0f85d63612b14c6742141
Montaha Bouezzeddine <montahabouezzed@gmail.com>
2026-04-18 01:15:54 +0200

    Add query to calculate average sensor values by component

--- sql/analysis/03_average_sensor_values_by_component.sql
+++ sql/analysis/03_average_sensor_values_by_component.sql
@@ -1,1 +1,10 @@
+SELECT 
+    c.component_name,
+    sr.sensor_type,
+    AVG(sr.value) AS avg_value
+FROM sensor_readings sr
+JOIN test_runs tr ON sr.test_run_id = tr.test_run_id
+JOIN components c ON tr.component_id = c.component_id
+GROUP BY c.component_name, sr.sensor_type
+ORDER BY c.component_name, sr.sensor_type;
 
