b5a44b4f26b97b6c8f43d6cce8e8e771c5c256bf
Montaha Bouezzeddine <montahabouezzed@gmail.com>
2026-04-18 01:21:51 +0200

    Update 10_top_risky_components.sql

--- sql/analysis/10_top_risky_components.sql
+++ sql/analysis/10_top_risky_components.sql
@@ -1,1 +1,10 @@
+SELECT 
+    c.component_name,
+    COUNT(a.anomaly_id) AS anomaly_count
+FROM components c
+JOIN test_runs tr ON c.component_id = tr.component_id
+JOIN anomalies a ON tr.test_run_id = a.test_run_id
+GROUP BY c.component_name
+ORDER BY anomaly_count DESC
+LIMIT 5;
 
