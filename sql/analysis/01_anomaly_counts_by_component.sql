181b024f974a4f9a801a84c58112c5fb7eed44bb
Montaha Bouezzeddine <montahabouezzed@gmail.com>
2026-04-18 01:14:44 +0200

    Add SQL query for anomaly counts by component

--- sql/analysis/01_anomaly_counts_by_component.sql
+++ sql/analysis/01_anomaly_counts_by_component.sql
@@ -1,1 +1,9 @@
+SELECT 
+    c.component_name,
+    COUNT(a.anomaly_id) AS anomaly_count
+FROM anomalies a
+JOIN test_runs tr ON a.test_run_id = tr.test_run_id
+JOIN components c ON tr.component_id = c.component_id
+GROUP BY c.component_name
+ORDER BY anomaly_count DESC;
 
