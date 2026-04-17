cb3dd957269e18272deff34516b3eef0a989d648
Montaha Bouezzeddine <montahabouezzed@gmail.com>
2026-04-18 01:20:38 +0200

    Add SQL query for vehicle performance overview

--- sql/analysis/07_vehicle_performance_overview.sql
+++ sql/analysis/07_vehicle_performance_overview.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    v.model,
+    COUNT(tr.test_run_id) AS total_runs,
+    COUNT(a.anomaly_id) AS total_anomalies,
+    ROUND(COUNT(a.anomaly_id) * 1.0 / COUNT(tr.test_run_id), 2) AS anomalies_per_run
+FROM vehicles v
+LEFT JOIN test_runs tr ON v.vehicle_id = tr.vehicle_id
+LEFT JOIN anomalies a ON tr.test_run_id = a.test_run_id
+GROUP BY v.model
+ORDER BY anomalies_per_run DESC;
