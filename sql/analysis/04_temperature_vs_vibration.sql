e0d38bc538d462db3abbc61ecda5de849e3462f9
Montaha Bouezzeddine <montahabouezzed@gmail.com>
2026-04-18 01:16:25 +0200

    Add SQL query for temperature vs vibration analysis

--- sql/analysis/04_temperature_vs_vibration.sql
+++ sql/analysis/04_temperature_vs_vibration.sql
@@ -1,1 +1,12 @@
+SELECT 
+    sr1.test_run_id,
+    AVG(sr1.value) AS avg_temperature,
+    AVG(sr2.value) AS avg_vibration
+FROM sensor_readings sr1
+JOIN sensor_readings sr2 
+    ON sr1.test_run_id = sr2.test_run_id
+   AND sr1.timestamp = sr2.timestamp
+WHERE sr1.sensor_type = 'temperature'
+  AND sr2.sensor_type = 'vibration'
+GROUP BY sr1.test_run_id;
 
